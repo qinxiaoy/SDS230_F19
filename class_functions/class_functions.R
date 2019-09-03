449e790c9accb176c903304b7b945d3fc8d4f259
emeyers <emeyers@hampshire.edu>
2019-09-03 19:44:31 -0400

    Adding some functions useful for downloading data and code related to the class

--- class_functions/class_functions.R
+++ class_functions/class_functions.R
@@ -13,3 +13,40 @@ download_class_data <- function(file_name){
 
 
 
+
+# A function to download the code for each class
+downlaod_class_code <- function(class_number){
+  
+  result = tryCatch({
+    downlaod_class_code_with_extension (class_number, is_R_file = FALSE)
+  },  warning = function(w) {
+    file_name <- paste0("class_", sprintf("%02d", class_number), ".Rmd")
+    file.remove(file_name)
+    downlaod_class_code_with_extension (class_number, is_R_file = TRUE)
+  }, finally = {
+  })
+  
+    
+}
+
+
+
+
+
+# A helper function to help get the code for each class based on the code's extension
+downlaod_class_code_with_extension <- function(class_number, is_R_file = FALSE){
+  
+  base_path <- "https://github.com/emeyers/SDS230_F19/blob/master/class_code/"
+  
+  extension <- ".Rmd"
+  if (is_R_file)
+    extension <- ".R"
+  
+  file_name <- paste0("class_", sprintf("%02d", class_number), extension)
+  
+  full_path <- paste0(base_path, file_name)
+  
+  download.file(full_path, file_name)
+  
+}
+
