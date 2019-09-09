297a79492988066fa8dd92bfa082e8891c390d23
emeyers <emeyers@hampshire.edu>
2019-09-09 18:05:21 -0400

    Added code so that the functions to download code, data and homework do not overwrite files that already exist but instead given an error message if one tries to download a file that already exists

--- class_functions/class_functions.R
+++ class_functions/class_functions.R
@@ -7,6 +7,13 @@ download_class_data <- function(file_name){
   
   full_path <- paste0(base_path, file_name)
   
+  if (file.exists(file_name)){
+    stop(paste("The data file you are trying to download", file_name,
+               "already exists. Please rename the file", file_name, 
+               "and then rerun this function to download a new copy"))
+  }
+  
+  
   download.file(full_path, file_name)
   
 }
@@ -24,17 +31,24 @@ download_class_code <- function(class_number){
     
     file_name <- paste0("class_", sprintf("%02d", class_number), ".Rmd")
     full_path <- paste0(base_path, file_name)
-    download.file(full_path, file_name)
   
   },  warning = function(w) {
     
     file.remove(file_name)
     file_name <- paste0("class_", sprintf("%02d", class_number), ".R")
     full_path <- paste0(base_path, file_name)
-    download.file(full_path, file_name)
-    
+
   }, finally = {
   })
+  
+  
+  if (file.exists(file_name)){
+    stop(paste("The class code file you are trying to download", file_name,
+               "already exists. Please rename the file", file_name, 
+               "and then rerun this function to download a new copy"))
+  }
+  
+  download.file(full_path, file_name)
   
 }
 
@@ -42,15 +56,21 @@ download_class_code <- function(class_number){
 
 
 
-# A function to download the code for each class
+# A function to download the homework
 download_homework <- function(homework_number){
   
   base_path <- "https://raw.githubusercontent.com/emeyers/SDS230_F19/master/homework/"
   
-
-    file_name <- paste0("homework_", sprintf("%02d", homework_number), ".Rmd")
-    full_path <- paste0(base_path, file_name)
-    download.file(full_path, file_name)
+  file_name <- paste0("homework_", sprintf("%02d", homework_number), ".Rmd")
+  
+  if (file.exists(file_name)){
+    stop(paste("The homework file you are trying to download", file_name,
+               "already exists. Please rename the file", file_name, 
+               "and then rerun this function to download a new copy"))
+  }
+  
+  full_path <- paste0(base_path, file_name)
+  download.file(full_path, file_name)
     
 }
 
